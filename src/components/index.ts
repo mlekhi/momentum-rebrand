7616e683e17db1ba40d2d990f7846a06aa0a08cb
mlekhi <maya.lekhi1@gmail.com>
2026-01-25 16:29:23 -0800

    adding community

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,7 @@
 export { Navbar } from "./Navbar";
 export { Hero } from "./Hero";
 export { Explore } from "./Explore";
+export { Community } from "./Community";
 export { Marquee } from "./Marquee";
 export { HowItWorks } from "./HowItWorks";
 export { CallToAction } from "./CallToAction";
