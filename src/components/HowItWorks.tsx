3f9d43559007f732cd63f5b5be7ebb72e90a79fc
mlekhi <maya.lekhi1@gmail.com>
2026-01-25 16:46:24 -0800

    adding no school job owrk

--- src/components/HowItWorks.tsx
+++ src/components/HowItWorks.tsx
@@ -1,8 +1,8 @@
 const steps = [
-  "50 minutes of work* time.",
-  "10 minute break.",
-  "50 minutes of work* time.",
-  "demos to wrap up!",
+  { text: "50 minutes of work* time.", hasAsterisk: true },
+  { text: "10 minute break.", hasAsterisk: false },
+  { text: "50 minutes of work* time.", hasAsterisk: true },
+  { text: "demos to wrap up!", hasAsterisk: false },
 ];
 
 export function HowItWorks() {
@@ -13,13 +13,19 @@ export function HowItWorks() {
       </h2>
       <div className="flex flex-col gap-8 pl-8 border-l-2 border-orange-500">
         {steps.map((item, i) => (
-          <div key={i} className="relative pl-8">
+          <div key={i} className="relative pl-8 group">
             <div className="absolute -left-[25px] top-1/2 -translate-y-1/2 w-3 h-3 rounded-full bg-orange-500" />
-            <p className="text-xl md:text-2xl">{item}</p>
+            <p className="text-xl md:text-2xl">
+              {item.text}
+              {item.hasAsterisk && (
+                <span className="ml-2 text-sm text-neutral-500 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                  *no school or job work allowed :)
+                </span>
+              )}
+            </p>
           </div>
         ))}
       </div>
     </section>
   );
 }
-
